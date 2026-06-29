571c44ab8da73ccfe291a5462d279c5dad19c2ee
zumichi0401 <zm26387@josai.ac.jp>
2026-06-29 11:57:12 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,3 +1,8 @@
-alert('Hello, Mizuchi!');
-alert(1 + 1);
-alert(Math.sqrt(1 + 1));
+alert('Hello, 自分の名前!');
+alert(1+1);
+alert(Math.sqrt(1+1));
+alert(2+3);
+alert('2'+'3');
+alert(Number('2')+Number('3'));
+alert(('2')+3);
+alert(Number('2')+3);
